a933c80b6d7d4beb1de18e80a15d28de0724e0df
David Raznick <kindly@gmail.com>
2019-02-28 23:31:29 +0000

    run collections in parallel

--- ocdskingfisherviews/cli/commands/field_counts.py
+++ ocdskingfisherviews/cli/commands/field_counts.py
@@ -1,5 +1,6 @@
 import sqlalchemy as sa
 from timeit import default_timer as timer
+import concurrent.futures
 
 import ocdskingfisherviews.cli.commands.base
 
@@ -19,14 +20,27 @@ field_count_query = '''
     group by collection_id, path;
 '''
 
+search_path_string = 'set search_path = views, public;'
+
 class FieldCountsCommand(ocdskingfisherviews.cli.commands.base.CLICommand):
     command = 'field-counts'
 
-    def run_command(self, args):
-        search_path_string = 'set search_path = views, public;'
-        engine = sa.create_engine(self.config.database_uri)
+    def run_collection(self, collection):
 
-        with engine.begin() as connection:
+        with self.engine.begin() as connection:
+            start = timer()
+            connection.execute(search_path_string)
+            print('processing collection: {}'.format(collection))
+            results = tuple(connection.execute(field_count_query, collection))
+            if results:
+                connection.execute('insert into field_counts_temp values (%s, %s, %s, %s, %s)', *results)
+            print('running time for collection {}: {}s'.format(collection, timer() - start))
+
+    def run_command(self, args):
+        self.engine = sa.create_engine(self.config.database_uri)
+        overall_start = timer()
+
+        with self.engine.begin() as connection:
             connection.execute(search_path_string)
 
             connection.execute('drop table if exists field_counts_temp')
@@ -35,19 +49,14 @@ class FieldCountsCommand(ocdskingfisherviews.cli.commands.base.CLICommand):
                 result['id'] for result in connection.execute('select id from selected_collections')
             ]
 
-            overall_start = timer()
-            for collection in selected_collections:
-                start = timer()
-                print('processing collection: {}'.format(collection))
-                results = connection.execute(field_count_query, collection)
-                if results:
-                    connection.execute('insert into field_counts_temp values (%s, %s, %s, %s, %s)', *results)
-                print('running time: {}s'.format(timer() - start))
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            futures = [executor.submit(self.run_collection, collection) for collection in selected_collections]
 
+            for future in concurrent.futures.as_completed(futures):
+                continue
+
+        with self.engine.begin() as connection:
             connection.execute('drop table if exists field_counts')
             connection.execute('alter table field_counts_temp rename to field_counts')
-            print('total running time: {}s'.format(timer() - start))
-
-
-
+            print('total running time: {}s'.format(timer() - overall_start))
 
