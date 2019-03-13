aa865d2930d09641a52a1dde988de0d7c4215016
David Raznick <kindly@gmail.com>
2019-03-13 11:03:02 +0000

    add way to add view that adds data, remove 1:1 data

--- sql/002-tmp_release_and_parties_downgrade.sql
+++ sql/002-tmp_release_and_parties_downgrade.sql
@@ -1,5 +1,8 @@
 set search_path = views, public;
 
+drop view if exists parties_summary;
 drop table if exists parties_summary;
+drop table if exists parties_summary_no_data;
 drop view if exists tmp_release_summary_with_release_data;
+
 drop table if exists tmp_release_summary;
