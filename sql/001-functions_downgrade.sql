b9b385d6cf1195c1d651e3f2873de2b4793dd141
David Raznick <kindly@gmail.com>
2019-05-03 11:54:27 +0100

    Add flatten with values function

--- sql/001-functions_downgrade.sql
+++ sql/001-functions_downgrade.sql
@@ -4,3 +4,4 @@ drop function if exists convert_to_numeric;
 drop function if exists convert_to_timestamp;
 drop function if exists common_comments;
 drop function if exists flatten;
+drop function if exists flatten_with_values;
