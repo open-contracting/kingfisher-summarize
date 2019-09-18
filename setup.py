1296c2661510b723b28035465d76c3e8aa3d2bc1
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-09-18 15:50:02 -0400

    Use setup.py instead of complicated system of requirements files. Use whitespace and quoting style consistent with other projects in setup.py.

--- setup.py
+++ setup.py
@@ -1,34 +1,41 @@
-#!/usr/bin/env python
-import io
-import os
-from distutils.core import setup
+from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    long_description = f.read()
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Import the README and use it as the long-description.
-with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
-
-setup(name='ocdskingfisher-views',
-      version='0.0.1',
-      description='A set of views of ocdskingfisher',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      author='Open Contracting Partnership, Open Data Services, Iniciativa Latinoamericana para los Datos Abiertos',
-      author_email='data@open-contracting.org',
-      url='https://open-contracting.org',
-      license='BSD',
-      packages=[
-            'ocdskingfisherviews',
-            'ocdskingfisherviews.cli',
-            'ocdskingfisherviews.cli.commands',
-            'ocdskingfisherviews.migrations',
-            'ocdskingfisherviews.migrations.versions',
-      ],
-      scripts=['ocdskingfisher-views-cli'],
-      package_data={'ocdskingfisher': [
-              'migrations/script.py.mako'
-          ]},
-      include_package_data=True
-      )
+setup(
+    name='ocdskingfisher-views',
+    version='0.0.1',
+    author='Open Contracting Partnership, Open Data Services, Iniciativa Latinoamericana para los Datos Abiertos',
+    author_email='data@open-contracting.org',
+    url='https://github.com/open-contracting/kingfisher-views',
+    description='A set of PostgreSQL views for OCDS Kingfisher',
+    license='BSD',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'alembic',
+        'logzero',
+        'pgpasslib',
+        'psycopg2',
+    ],
+    extras_require={
+        'test': [
+            'coveralls',
+            'pytest',
+            'pytest-cov',
+        ],
+    },
+    package_data={'ocdskingfisher': [
+        'migrations/script.py.mako'
+    ]},
+    include_package_data=True,
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    scripts=[
+        'ocdskingfisher-views-cli',
+    ],
+)
