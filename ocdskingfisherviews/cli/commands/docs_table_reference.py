a6658192f8f286d4db620accdb956ac383492a85
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-03-02 13:10:34 -0500

    Update docs-table-ref to work with collection-specific schemas

--- ocdskingfisherviews/cli/commands/docs_table_reference.py
+++ ocdskingfisherviews/cli/commands/docs_table_reference.py
@@ -14,19 +14,22 @@ column_info_query = '''
         pg_catalog.col_description(format('%%s.%%s',isc.table_schema,isc.table_name)::regclass::oid,
                                    isc.ordinal_position) as column_description
     FROM
-        information_schema.columns isc where table_schema='views' and lower(isc.table_name) = lower(%s);
+        information_schema.columns isc where table_schema=%s and lower(isc.table_name) = lower(%s);
 '''
 
 
 class DocsTableRefCommand(ocdskingfisherviews.cli.commands.base.CLICommand):
     command = 'docs-table-ref'
 
+    def configure_subparser(self, subparser):
+        subparser.add_argument("name", help="Name Of View")
+
     def run_command(self, args):
 
         dir_path = os.path.dirname(os.path.realpath(__file__))
 
         docs_path = os.path.join(dir_path, '../../../docs')
-        docs_csv = os.path.join(docs_path, 'view_definitions')
+        docs_csv = os.path.join(docs_path, 'definitions')
 
         sql_scripts_path = os.path.join(dir_path, '../../../sql')
         all_scripts = sorted(glob.glob(sql_scripts_path + '/*.sql'))
@@ -48,15 +51,19 @@ class DocsTableRefCommand(ocdskingfisherviews.cli.commands.base.CLICommand):
 
         engine = sa.create_engine(self.config.database_uri)
 
+        headers = {
+            'column_name': 'Column Name',
+            'data_type': 'Data Type',
+            'column_description': 'Description',
+        }
+
         for table in all_tables:
             csv_file_name = os.path.join(docs_csv, table + '.csv')
             with engine.begin() as connection, open(csv_file_name, 'w+') as output:
-                writer = csv.DictWriter(output, ['column_name', 'data_type', 'column_description'])
-                writer.writerow({'column_name': 'Column Name',
-                                 'data_type': 'Data Type',
-                                 'column_description': 'Description'})
+                writer = csv.DictWriter(output, headers.keys(), lineterminator='\n')
+                writer.writerow(headers)
 
-                for result in connection.execute(column_info_query, [table]):
+                for result in connection.execute(column_info_query, ['view_data_' + args.name, table]):
                     result_dict = dict(result)
                     if 'timestamp' in result_dict['data_type']:
                         result_dict['data_type'] = 'timestamp'
